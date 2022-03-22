20ba87f8a24869de935e6646faedc8c722d14795
akarsh02 <akarsh@zeroswap.io>
2022-03-22 15:18:55 +0530

    contract to create own token

--- src/Contract/simple.sol
+++ src/Contract/simple.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Simple_Token is ERC20 {
-  constructor(uint256 initialSupply) ERC20("Simple_Token", "ST") {
+  constructor(uint256 initialSupply) ERC20("Simple_Token", "ST1") {
     _mint(msg.sender, initialSupply);
   }
 
